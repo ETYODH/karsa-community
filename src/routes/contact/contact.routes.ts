31d4b8fc56516fed9355624ad46aa6c46f6fd38a
ETYODH <arkanafadla82@gmail.com>
2026-09-07 23:42:17 +0700

    Fix contact route module import

--- src/routes/contact/contact.routes.ts
+++ src/routes/contact/contact.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
+
 import {
   getContacts,
   createContact,
-} from "../../controller/contact/contact.controller";
+} from "../../controller/contact/contact.controller.js";
 
 const router = Router();
 
 router.get("/", getContacts);
+
 router.post("/", createContact);
 
 export default router;
