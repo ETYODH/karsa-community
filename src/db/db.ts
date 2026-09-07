ec5bd2bfce6974ddea29b895480b1cb497e7f4d9
ETYODH <arkanafadla82@gmail.com>
2026-09-08 00:11:36 +0700

    Fix schema module import

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
+
 import { drizzle } from "drizzle-orm/mysql2";
+
 import mysql from "mysql2/promise";
-import * as schema from "./schema";
+
+import * as schema from "./schema.js";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
